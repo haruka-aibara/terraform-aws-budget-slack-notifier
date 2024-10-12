fc58eeafcc2b6624a4330118191193296c8ea49a
haruka-aibara <haruka_aibara@outlook.com>
2024-10-12 16:10:35 +0900

    set actual budgets limit amount

--- main_budgets.tf
+++ main_budgets.tf
@@ -1,14 +1,14 @@
 resource "aws_budgets_budget" "all_daily" {
   name         = "budget-all-daily"
   budget_type  = "COST"
-  limit_amount = 1
+  limit_amount = local.budgets_limit_ammout
   limit_unit   = "USD"
   time_unit    = "DAILY"
 
   notification {
     notification_type         = "ACTUAL"
     comparison_operator       = "GREATER_THAN"
-    threshold                 = 0.1
+    threshold                 = local.budgets_limit_ammout
     threshold_type            = "ABSOLUTE_VALUE"
     subscriber_sns_topic_arns = [aws_sns_topic.notify_slack.arn]
   }
